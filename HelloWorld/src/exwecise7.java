0a5080edaf8de9e64769600a94658b577898422f
jihyeong-chae <jh1420@cu.ac.kr>
2019-04-02 16:38:05 +0900

    4/2_exercise7

---
+++ HelloWorld/src/exwecise7.java
@@ -0,0 +1,18 @@
+
+public class exwecise7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 0;
+		int array[] = {1,5,3,8,2};
+		int i;
+		for(i=0; i < array.length; i++) {
+			if (max < array[i]) {
+				max = array[i];
+				System.out.println("max: " +max);
+			}
+		}
+	System.out.println("max: " +max);
+	}
+
+}
